2eccb7de452527773b1df4d322406c1ec9bfad01
LucasBraz <lucasbz69@gmail.com>
2022-05-06 16:47:27 +0200

    Update Qlearning.py

--- Qlearning.py
+++ Qlearning.py
@@ -8,15 +8,25 @@ class QlearningAgent:
 
     Parameters:
         epsilon: float, in [0, 1]. This is a value between 0-1 that indicates the
-            probability of making a random action instead of the optimal action
+            probability of making a random action instead of the greedy action
             at any given time.
 
-        TODO
-
+        learningRate: float, in [0, 1]. Setting it to 0 means that the Q-values are
+            never updated, hence nothing is learned. Setting a high value such as 0.9 
+            means that learning can occur quickly. 
+        
+        discountFactor: float, in [0, 1]. This models the fact that future rewards are
+            worth less than immediate rewards. Setting it to 0 means that the agent
+            will only learn from immediate rewards. Setting it to 1 means that the
+            agent will learn from all rewards equally.
     '''
-    def __init__(self, epsilon=0.2, player='X'):
+    
+    def __init__(self, epsilon=0.2, player='X', learningRate=0.05, discountFactor=0.99):
         self.epsilon = epsilon
-        self.player = player # 'x' or 'O'
+        self.player = player # 'X' or 'O'
+        self.learningRate = learningRate
+        self.discountFactor = discountFactor
+        self.Q = {} # use get(key, default)
 
     def set_player(self, player = 'X', j=-1):
         self.player = player
@@ -31,130 +41,6 @@ class QlearningAgent:
             if grid[pos] == 0:
                 avail.append(pos)
         return avail
-
-    def center(self, grid):
-        '''
-        Pick the center if its available,
-        if it's the first step of the game, center or corner are all optimial.
-        '''
-        if np.abs(grid).sum() == 0:
-            # first step of the game
-            return [(1, 1)] + self.corner(grid)
-
-        return [(1, 1)] if grid[1, 1] == 0 else []
-
-    def corner(self, grid):
-        ''' Pick empty corners to move '''
-        corner = [(0, 0), (0, 2), (2, 0), (2, 2)]
-        cn = []
-        # First, pick opposite corner of opponent if it's available
-        for i in range(4):
-            if grid[corner[i]] == 0 and grid[corner[3 - i]] != 0:
-                cn.append(corner[i])
-        if cn != []:
-            return cn
-        else:
-            for idx in corner:
-                if grid[idx] == 0:
-                    cn.append(idx)
-            return cn
-
-    def side(self, grid):
-        ''' Pick empty sides to move'''
-        rt = []
-        for idx in [(0, 1), (1, 0), (1, 2), (2, 1)]:
-            if grid[idx] == 0:
-                rt.append(idx)
-        return rt
-
-    def win(self, grid, val=None):
-        ''' Pick all positions that player will win after taking it'''
-        if val is None:
-            val = 1 if self.player == 'X' else -1
-
-        towin = []
-        # check all positions
-        for pos in self.empty(grid):
-            grid_ = np.copy(grid)
-            grid_[pos] = val
-            if self.checkWin(grid_, val):
-                towin.append(pos)
-
-        return towin
-
-    def blockWin(self, grid):
-        ''' Find the win positions of opponent and block it'''
-        oppon_val = -1 if self.player == 'X' else 1
-        return self.win(grid, oppon_val)
-
-    def fork(self, grid, val=None):
-        ''' Find a fork opportunity that the player will have two positions to win'''
-        if val is None:
-            val = 1 if self.player == 'X' else -1
-
-        tofork = []
-        # check all positions
-        for pos in self.empty(grid):
-            grid_ = np.copy(grid)
-            grid_[pos] = val
-            if self.checkFork(grid_, val):
-                tofork.append(pos)
-
-        return tofork
-
-    def blockFork(self, grid):
-        ''' Block the opponent's fork.
-            If there is only one possible fork from opponent, block it.
-            Otherwise, player should force opponent to block win by making two in a row or column
-            Amomg all possible force win positions, choose positions in opponent's fork in prior
-        '''
-        oppon_val = -1 if self.player == 'X' else 1
-        oppon_fork = self.fork(grid, oppon_val)
-        if len(oppon_fork) <= 1:
-            return oppon_fork
-
-        # force the opponent to block win
-        force_blockwin = []
-        val = 1 if self.player == 'X' else -1
-        for pos in self.empty(grid):
-            grid_ = np.copy(grid)
-            grid_[pos] = val
-            if np.any(np.sum(grid_, axis=0) == val*2) or np.any(np.sum(grid_, axis=1) == val*2):
-                force_blockwin.append(pos)
-        force_blockwin_prior = []
-        for pos in force_blockwin:
-            if pos in oppon_fork:
-                force_blockwin_prior.append(pos)
-
-        return force_blockwin_prior if force_blockwin_prior != [] else force_blockwin
-
-    def checkWin(self, grid, val=None):
-        # check whether the player corresponding to the val will win
-        if val is None:
-            val = 1 if self.player == 'X' else -1
-        target = 3 * val
-        # check rows and cols
-        if np.any(np.sum(grid, axis=0) == target) or np.any(np.sum(grid, axis=1) == target):
-            return True
-        # check diagnols
-        elif grid[[0,1,2],[0,1,2]].sum() == target or grid[[0,1,2],[2,1,0]].sum() == target:
-            return True
-        else:
-            return False
-
-    def checkFork(self, grid, val=None):
-        # check whether the player corresponding to the val will fork
-        if val is None:
-            val = 1 if self.player == 'X' else -1
-        target = 2 * val
-        # check rows and cols
-        rows = (np.sum(grid, axis=0) == target).sum()
-        cols = (np.sum(grid, axis=1) == target).sum()
-        diags = (grid[[0,1,2],[0,1,2]].sum() == target) + (grid[[0,1,2],[2,1,0]].sum() == target)
-        if (rows + cols + diags) >= 2:
-            return True
-        else:
-            return False
 
     def randomMove(self, grid):
         """ Chose a random move from the available options. """
@@ -171,37 +57,6 @@ class QlearningAgent:
         # whether move in random or not
         if random.random() < self.epsilon:
             return self.randomMove(grid)
-
-        ### optimial policies
-
-        # Win
-        win = self.win(grid)
-        if win != []:
-            return win[random.randint(0, len(win)-1)]
-        # Block win
-        block_win = self.blockWin(grid)
-        if block_win != []:
-            return block_win[random.randint(0, len(block_win)-1)]
-        # Fork
-        fork = self.fork(grid)
-        if fork != []:
-            return fork[random.randint(0, len(fork)-1)]
-        # Block fork
-        block_fork = self.blockFork(grid)
-        if block_fork != []:
-            return block_fork[random.randint(0, len(block_fork)-1)]
-        # Center
-        center = self.center(grid)
-        if center != []:
-            return center[random.randint(0, len(center)-1)]
-        # Corner
-        corner = self.corner(grid)
-        if corner != []:
-            return corner[random.randint(0, len(corner)-1)]
-        # Side
-        side = self.side(grid)
-        if side != []:
-            return side[random.randint(0, len(side)-1)]
 
         # random move
         return self.randomMove(grid)
