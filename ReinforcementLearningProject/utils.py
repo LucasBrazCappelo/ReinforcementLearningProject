5b8af0529e99d26110df8115ccddc982941b9406
LucasBraz <lucasbz69@gmail.com>
2023-08-29 23:30:34 +0200

    Revert "update after precommit"
    This reverts commit 565065d29c0f38526fe462c6d69f8694587d6c49.

--- ReinforcementLearningProject/utils.py
+++ ReinforcementLearningProject/utils.py
@@ -8,9 +8,8 @@ import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as nn_functional
-from tqdm.notebook import trange
-
 from tic_env import OptimalPlayer, TictactoeEnv
+from tqdm.notebook import trange
 
 
 class Agent(ABC):
